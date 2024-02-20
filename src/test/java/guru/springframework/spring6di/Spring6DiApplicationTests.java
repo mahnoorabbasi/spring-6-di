b9150685e98f1963108e27f4488557afa1abff82
mahnoor <mabbasi@xavor.com>
2024-02-20 16:13:34 +0500

    tested spring context

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    //can also test directyl the controller
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutoWireOfController(){
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx(){
+        MyController myController=applicationContext.getBean(MyController.class);
+        System.out.println(myController.sayHello());
+    }
 
     @Test
     void contextLoads() {
